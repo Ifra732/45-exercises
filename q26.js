313b22b8d04e2970999d6d3c7e1684a0a1804d15
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:38:52 -0700

    Add files via upload

---
+++ q26.js
@@ -0,0 +1,33 @@
+var alienColour = "green";
+//using if and else statement
+if (alienColour === "green") {
+    console.log("you shoot down green alien yu earned 5 points.");
+}
+else if (alienColour === "yellow") {
+    console.log("you shoot down yelow alien you earned 10 points.");
+}
+else if (alienColour === "red") {
+    console.log("you shoot down red alien you earned 15 points.");
+}
+//second version
+var alienColour2 = "yellow";
+if (alienColour2 === "green") {
+    console.log("you shot down green alien you earned 5 points.");
+}
+else if (alienColour2 === "yellow") {
+    console.log("you shot down yellow alien you earned 10 points.");
+}
+else if (alienColour2 === "red") {
+    console.log("you shot down ed alien you earned 15points.");
+}
+//version 3
+var alienColour3 = "red";
+if (alienColour3 === "green") {
+    console.log("you shot down green alien you earned 5 points.");
+}
+else if (alienColour3 === "yellow") {
+    console.log("you shot down yellow alien you earned 10 points.");
+}
+else if (alienColour3 === "red") {
+    console.log("you shot down red alien you earned 15 points.");
+}
