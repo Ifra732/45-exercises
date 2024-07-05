5f0b2ce03fe2c762e8193c000ca8e69b958fb80c
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:30:48 -0700

    Add files via upload

---
+++ q11.js
@@ -0,0 +1,5 @@
+var names = ["binish", "aqsa", "fiza"];
+var message = "would you like to learn some python today";
+console.log(names[0] + "  " + message);
+console.log(names[1] + "  " + message);
+console.log(names[2] + "  " + message);
