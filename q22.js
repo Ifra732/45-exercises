23fe542571e1fdbe8852cf8f37a66745917928af
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:40:17 -0700

    Add files via upload

---
+++ q22.js
@@ -0,0 +1,33 @@
+//making a variable
+var five = 5;
+var ten = 10;
+//test
+console.log("test1:five is equal to 5?");
+console.log(five == 5);
+//test 2
+console.log(" \n test2:ten is equal to 10?");
+console.log(ten == 10);
+//test 3
+console.log(" \n test3: 5 is not equal to 10");
+console.log(five != ten);
+//test 4
+console.log("\n test4:ten is greater then 5");
+console.log(ten > five);
+//test 5
+console.log("\n test5: 5 is smaller then 10");
+console.log(five < 10);
+//test 6
+console.log("\ntest6: 10 is smaller then 5");
+console.log(ten < 5);
+//test 7
+console.log("\ntest7:5 is equal to ten");
+console.log(five == ten);
+//test8
+console.log("\n test8 ten is not equal to 10 ");
+console.log(ten != 10);
+//test 9
+console.log("\n test 9 five is greter then 10");
+console.log(five > ten);
+//test 10
+console.log("\n test10:ten is greater then 50");
+console.log(ten > 50);
