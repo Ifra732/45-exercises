88ea753d716c5c3bf0211602c4693ae19ee4505d
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:43:12 -0700

    Add files via upload

---
+++ q28.js
@@ -0,0 +1,14 @@
+//create a Array
+var favouritefrouts = ["apple", "mango", "banana",];
+if (favouritefrouts.includes("mango")) {
+    console.log("i really like mango.");
+}
+if (favouritefrouts.includes("apple")) {
+    console.log("i really like apple.");
+}
+if (favouritefrouts.includes("orange")) {
+    console.log("i really like orange.");
+}
+if (favouritefrouts.includes("banana")) {
+    console.log("i really like banana");
+}
