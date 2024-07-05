166428c6709cb44f3bd97e7955708218215a1675
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:21:42 -0700

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,7 @@
+var personname = "Ifra";
+//lower case
+console.log(personname.toLocaleLowerCase());
+//upper case
+console.log(personname.toUpperCase());
+//title case
+console.log(personname.charAt(0).toUpperCase() + personname.slice(1, 4));
