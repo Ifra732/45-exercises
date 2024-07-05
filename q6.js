4738e1610da9711c2ef4a0ddfe913db022f9037b
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:24:37 -0700

    Add files via upload

---
+++ q6.js
@@ -0,0 +1,12 @@
+//Addition
+var Addition = 5 + 3;
+console.log("Addition", Addition);
+//substraction
+var substraction = 10 - 2;
+console.log("substraction", substraction);
+//Multiplication
+var Multiplication = 4 * 2;
+console.log("Multiplication", Multiplication);
+//Division
+var Division = 16 / 2;
+console.log("Division", Division);
