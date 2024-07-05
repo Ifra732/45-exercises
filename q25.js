79681d0443900db2165460a430c004fdd41fed1f
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:37:35 -0700

    Add files via upload

---
+++ q25.js
@@ -0,0 +1,14 @@
+var alienColour = "green";
+if (alienColour === "green") {
+    console.log("player just earned 5 points for shooting alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+//second version
+if (alienColour === "blue") {
+    console.log("i am come from if statement.");
+}
+else {
+    console.log("i am come from else statement.");
+}
