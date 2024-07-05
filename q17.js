cbd2ccaf744fc1f09f662d649dc7c6364dd5e5f3
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:34:57 -0700

    Add files via upload

---
+++ q17.js
@@ -0,0 +1,13 @@
+var palaceToVisit = ["Hongkong", "America", "Pakistan", "Iran"];
+// console.log();palaceToVisit
+// console.log(palaceToVisit.sort());
+// console.log(palaceToVisit);
+// console.log(palaceToVisit.sort().reverse());
+// console.log(palaceToVisit);
+// console.log(palaceToVisit.reverse());
+// console.log(palaceToVisit.reverse());
+var sortedArray = ["Hongkong", "America", "Pakistan", "Iran"];
+sortedArray.sort();
+console.log(sortedArray);
+sortedArray.reverse();
+console.log(sortedArray);
