b98ec33c29f96256ae8eca82d9e2cc9d8877dd68
Ifra732 <ifrariaz732@gmail.com>
2024-07-05 04:41:06 -0700

    Add files via upload

---
+++ q24.js
@@ -0,0 +1,9 @@
+//Define variable
+var alienColour = "Black";
+if (alienColour === "Black") {
+    //code Block
+    console.log(" alienColour is Black player just earned five points");
+}
+if (alienColour === "white") {
+    console.log("alienColour is white");
+}
